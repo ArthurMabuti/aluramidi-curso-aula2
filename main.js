c0df00c51267ca0a03cc0f7dff40586f36b39b5a
ArthurMabuti <arthurmabuti@hotmail.com>
2023-02-27 19:42:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//Para
+for(let i = 0; i < listaDeTeclas.length; i++){
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //Template String
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(event) {
+        if(event.key === "Enter" || event.code === "Space"){
+            tecla.classList.add('ativa');
+        } 
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
+
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
